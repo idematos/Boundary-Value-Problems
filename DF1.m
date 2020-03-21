9a2c33612cfacb8de298ed03e2146f512dfbb133
Isabela Matos <isabelavdmatos@gmail.com>
2020-03-21 18:07:28 -0300

    Update DF1.m

--- DF1.m
+++ DF1.m
@@ -1,13 +1,11 @@
-# Isabela Vegini de Matos 8531362
-
 a = 1;
 b = 5;
 alpha = 0;
 beta = log(5);
 N = 15;
-TOL = 0.001; # erro de newton 
+TOL = 0.001; # Newton`s error
 M = 100;
 
-f = @(x,w,z) -(z^2)-w+log(x);
+f = @(x,w,z) - (z^2) - w + log(x);
 
-W = diferencasfinitas(a,b,alpha,beta,N,TOL,M,f);
+W = finiteDifferences(a,b,alpha,beta,N,TOL,M,f);
